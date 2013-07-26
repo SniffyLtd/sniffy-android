618c818f9c42a59f89417e5879cdabc73c5fae1d
Tomasz Landowski <landowskitomasz@gmail.com>
2013-07-26 23:18:40 +0200

    SC# Open menu prototype/

--- src/com/brand/applicationname/android/ProductDetailsActivity.java
+++ src/com/brand/applicationname/android/ProductDetailsActivity.java
@@ -1,17 +1,47 @@
 package com.brand.applicationname.android;
 
+import android.R.bool;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Fragment;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class ProductDetailsActivity extends Activity {
 
 	public static final String PRODUCT_PARAMETER = "product";
+	
+	private Button expandButton;
+	
+	private View menu;
+	
+	private boolean menuOpened = true;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_product_details);
+		expandButton = (Button)findViewById(R.id.expand_button);
+		menu = findViewById(R.id.fragment_product_menu);
+		expandButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				expandMenu();
+			}
+		});
+	}
+
+	protected void expandMenu() {
+		if(menuOpened){
+			menu.setVisibility(View.GONE);
+		}
+		else{
+			menu.setVisibility(View.VISIBLE);
+		}
+		menuOpened= !menuOpened;
 	}
 
 	@Override
